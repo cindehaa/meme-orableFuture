11d6fb654d1d8bd161cd1ae451fe33470c2d2204
Cindy Li <rin.mp4@gmail.com>
2023-09-23 11:33:01 -0400

    map thing kind of working

--- map-app/src/views/HomeView.vue
+++ map-app/src/views/HomeView.vue
@@ -2,14 +2,8 @@
 import MapContainer from '../components/MapContainer.vue'
 </script>
 
-<!-- <template>
-  <main>
-    <TheWelcome />
-  </main>
-</template> -->
-
 <template>
   <main>
-    <MapContainer />
+    <MapContainer style="width: 50%; height: 50%; min-height: 50%; min-width: 50%;"/>
   </main>
 </template>
