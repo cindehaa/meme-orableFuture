95f054c15acf1c8a7722bb54dc71d0131849088a
Cindy Li <rin.mp4@gmail.com>
2023-09-23 11:18:44 -0400

    initial vue set-up

---
+++ map-app/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MapContainer from '../components/MapContainer.vue'
+</script>
+
+<!-- <template>
+  <main>
+    <TheWelcome />
+  </main>
+</template> -->
+
+<template>
+  <main>
+    <MapContainer />
+  </main>
+</template>
