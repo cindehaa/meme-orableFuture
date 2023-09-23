e3441a45202ff5e0522e816d08f2d5919dcd7e34
Cindy Li <rin.mp4@gmail.com>
2023-09-23 16:06:45 -0400

    for now

--- map-app/src/components/CountryInfo.vue
+++ map-app/src/components/CountryInfo.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="card-container">
-        
-    </div>
+        <div class="country-name">
+            <h1>Placeholder</h1>
+        </div>
+        <div class="sustainable-stat">
+            <p>Placeholder</p>
+        </div>
+        <div class="recommended-activity">
+            
+        </div>
 </template>
